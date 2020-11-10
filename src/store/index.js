34adf8bc5471a2660310579d4bd7e23875c0ed3c
Xiang Ji <hi@xiangji.me>
2020-11-11 00:06:21 +0100

    Use a simple file for config

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,12 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
-import * as config from './modules/config';
 import * as interactiveExperiment from './modules/interactiveExperiment';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
-    config,
     interactiveExperiment
   }
 });
