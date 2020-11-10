f8bc7719f20baeb598d12bb71f7bbae4eff8c507
Xiang Ji <hi@xiangji.me>
2020-11-11 00:07:50 +0100

    Fix config.js

--- src/config.js
+++ src/config.js
@@ -1,13 +1,3 @@
-// export const // experimentID: '171';
-// export const experimentID = '2';
-// export const // serverAppURL: 'https://magpie-demo.herokuapp.com/api/submit_experiment/';
-// export const serverAppURL = 'http://localhost:4000/api/submit_experiment/';
-// export const // socketURL: 'wss://magpie-demo.herokuapp.com/socket';
-// export const socketURL = 'ws://localhost:4000/socket';
-// export const deployMethod = 'debug';
-// export const contact_email =  'YOUREMAIL@wherelifeisgreat.you';
-// export const prolificURL = 'https://app.prolific.ac/submissions/complete?cc=EXAMPLE1234'
-
 export default {
   // experimentID: '171',
   experimentID: '2',
