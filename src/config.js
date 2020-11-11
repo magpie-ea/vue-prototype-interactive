3ffe26d67a603ffe0e0064c5304006c56013dae9
Xiang Ji <hi@xiangji.me>
2020-11-11 17:10:04 +0100

    Use actual magpie-demo server

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 export default {
-  // experimentID: '171',
-  experimentID: '2',
-  // serverAppURL: 'https://magpie-demo.herokuapp.com/api/submit_experiment/',
-  serverAppURL: 'http://localhost:4000/api/submit_experiment/',
-  // socketURL: 'wss://magpie-demo.herokuapp.com/socket',
-  socketURL: 'ws://localhost:4000/socket',
+  experimentID: '171',
+  // experimentID: '2',
+  serverAppURL: 'https://magpie-demo.herokuapp.com/api/submit_experiment/',
+  // serverAppURL: 'http://localhost:4000/api/submit_experiment/',
+  socketURL: 'wss://magpie-demo.herokuapp.com/socket',
+  // socketURL: 'ws://localhost:4000/socket',
   deployMethod: 'debug',
   contact_email: 'YOUREMAIL@wherelifeisgreat.you',
   prolificURL: 'https://app.prolific.ac/submissions/complete?cc=EXAMPLE1234'
